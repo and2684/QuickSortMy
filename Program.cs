821ee78188d9d63e45e00dac49a94b7d9bd85455
aaaa6226 <aaaa6226@STARIKAN>
2023-09-02 00:37:21 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿namespace QuickSortMy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new int[] { 11, 96, 14, 75, 222, 1, 14, 223, 25, 45, 6, 20 };
+
+            var sortedArray = QuickSortMy(array, 0, array.Length - 1);
+
+            Console.WriteLine($"Sorted array: {String.Join(", ", sortedArray)}");
+            Console.ReadKey();
+        }
+
+        private static int[] QuickSortMy(int[] array, int min, int max)
+        {
+            if (min >= max)
+            {
+                return array;
+            }
+
+            var pivot = GetPivot(array, min, max);
+
+            QuickSortMy(array, min, pivot - 1);
+            QuickSortMy(array, pivot + 1, max);
+
+            return array;
+        }
+
+        private static void Swap(ref int item1, ref int item2)
+        {
+            int t = item1;
+            item1 = item2;
+            item2 = t;
+        }
+
+        private static int GetPivot(int[] array, int min, int max)
+        {
+            var pivot = min - 1;
+            for (var i = min; i <= max; i++)
+            {
+                if (array[i] < array[max])
+                {
+                    pivot++;
+                    Swap(ref array[pivot], ref array[i]);
+                }
+            }
+
+            pivot++;
+            Swap(ref array[pivot], ref array[max]);
+            return pivot;
+        }
+    }
+}
